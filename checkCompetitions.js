719b0e72a602b6ccff077f83ce763e71fd07c865
Raulshadow <50181593+Raulshadow@users.noreply.github.com>
2024-08-16 16:34:40 -0300

    Mudança de IP do servidor de execução

--- checkCompetitions.js
+++ checkCompetitions.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const checkCompeticoes = async (dao) => {
     console.log('Checando competições');
-    const url1 = 'http://4.228.0.168:3001/executar';
+    const url1 = 'http://4.157.222.169:3001/executar';
     await dao.get_competicoes_ontem().then(response => {
         for (const competicao of response) {
             console.log('Executando competição:', competicao.ID);
